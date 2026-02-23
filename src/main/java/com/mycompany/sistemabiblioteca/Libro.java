014dac2d0f156266d0f7810476dbe2b780590f45
Alfredo Bautista <adbautista@unicesar.edu.co>
2026-02-22 21:52:09 -0500

    feat: agregar constructor y encapsulamiento a clases libro

--- src/main/java/com/mycompany/sistemabiblioteca/Libro.java
+++ src/main/java/com/mycompany/sistemabiblioteca/Libro.java
@@ -10,4 +10,39 @@ package com.mycompany.sistemabiblioteca;
  */
 public class Libro {
     
+    //atributos
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anioPublicacion;
+    private boolean disponible;
+    
+    //Constructor 
+    public Libro(String isbn, String titulo, String autor,String genero, int anioPublicacion) {
+    this.isbn            = isbn;
+    this.titulo          = titulo;
+    this.autor           = autor;
+    this.genero          = genero;
+    this.anioPublicacion = anioPublicacion;
+    this.disponible      = true;
+}
+    //Getters
+    public String getIsbn()            { return isbn; }
+    public String getTitulo()          { return titulo; }
+    public String getAutor()           { return autor; }
+    public String getGenero()          { return genero; }
+    public int    getAnioPublicacion() { return anioPublicacion; }
+    public boolean isDisponible()      { return disponible; }
+
+    //Setters
+    public void setIsbn(String isbn)                     { this.isbn = isbn; }
+    public void setTitulo(String titulo)                 { this.titulo = titulo; }
+    public void setAutor(String autor)                   { this.autor = autor; }
+    public void setGenero(String genero)                 { this.genero = genero; }
+    public void setAnioPublicacion(int anioPublicacion)  { this.anioPublicacion = anioPublicacion; }
+    public void setDisponible(boolean disponible)        { this.disponible = disponible; }
+        
+    
+    
 }
